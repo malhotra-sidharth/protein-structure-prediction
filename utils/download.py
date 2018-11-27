61f8966b6851208fdcb90f706271a7258f391857
Robin Walters <rsfwalters@gmail.com>
2018-11-27 17:29:56 -0500

    adding utilities for pdb download

---
+++ utils/download.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os.path
+
+f = open('mousepdb.txt', "r")
+lines = f.readlines()
+f.close()
+
+c=0
+
+for line in lines:
+	c += 1
+	st=(line.strip()).lower()
+	cmd="curl -O https://files.rcsb.org/download/"+st+".pdb"
+	print(cmd)
+	os.system(cmd)
+#	print(cmd)
+#	if (c > 5):
+#		break
+	print(c)
